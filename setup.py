f7b84e905656ae51979b417f7bc4fe4659dc6517
Low Kian Seong <kianseong@gmail.com>
2014-08-09 22:52:26 +0800

    Update setup.py
    Adding more metadata to setup.py for more info on `pypi`

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 setup(name='texit',
-	version='1.0',
-	py_modules=['texit'],
-	)
+      version='1.0',
+      py_modules=['texit'],
+      description='Python API bindings for texit.',
+      url='https://github.com/texit/texit-python',
+      )
